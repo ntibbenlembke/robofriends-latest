6f7ff6746ca6f6d5bd8b33da8cea25d1e0853fed
Noah Tibben-Lembke <ntibbenlembke@gmail.com>
2025-08-25 20:44:07 -0500

    Set up Store and Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
+import { Provider } from 'react-redux'
+import { legacy_createStore as createStore } from 'redux'
 import { createRoot } from "react-dom/client";
 import './index.css'; 
 import App from './containers/App';
 import 'tachyons';
+import { searchRobots } from './reducers';
 // import registerServiceWorker from './registerServiceWorker';
 
-
+const store = createStore(searchRobots)
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
